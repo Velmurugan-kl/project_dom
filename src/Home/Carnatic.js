3973ede42a88d94b4905303029a140192b48201b
Surekha2804 <22ece.surekha.s@skct.edu.in>
2024-02-21 09:36:25 +0530

    first commit

---
+++ src/Home/Carnatic.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+const Carnatic = () => {
+  return (
+    <div>
+      <center>
+    
+      <h1>Music Beginners ? Start Here!</h1>
+      </center>
+      <br></br>
+      <div class="cont">
+      <h4>
+Carnatic music is a classical music tradition that originated in southern India, particularly in the states of Tamil Nadu, Karnataka, Andhra Pradesh, and Telangana. It is one of the oldest and most sophisticated systems of music in the world, with roots dating back thousands of years.
+Characterized by its intricate melodies, rhythmic complexity, and emotive expression, Carnatic music is primarily vocal-centric, although it also encompasses a wide range of instruments including the violin, veena, flute, and mridangam.Carnatic music has a rich cultural heritage and plays a central role in religious and social ceremonies, as well as in concert settings. It is deeply intertwined with Hindu devotional practices and has evolved over centuries through the contributions of numerous composers and musicians </h4>
+<br></br>
+</div>
+      <div class="card-group">
+  <div class="card">
+    <img src="https://t3.ftcdn.net/jpg/06/36/14/40/360_F_636144050_4uikRcGLjyrHC7lvjizZo0Q9JmY72lgn.jpg" class="card-img-top" width="100" height="280" alt="..." ></img>
+    <div class="card-body">
+      <h5 class="card-title">Violin</h5>
+      <p class="card-text">Playing the violin is a deeply rewarding and enriching experience that combines technical skill with artistic expression</p>
+      
+    </div>
+  </div>
+  
+  <div class="card">
+    <img src="https://img-c.udemycdn.com/course/750x422/2458620_a6fd.jpg" class="card-img-top" width="100" alt="..."></img>
+    <div class="card-body">
+      <h5 class="card-title">Veena</h5>
+      <p class="card-text">The veena is a traditional Indian string instrument renowned for its melodic richness and cultural significance.</p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="https://assets.cntraveller.in/photos/60ba06fcce82080a5bb7333c/16:9/w_1024%2Cc_limit/Lead-Getty.jpg" class="card-img-top" alt="..."></img>
+    <div class="card-body">
+      <h5 class="card-title">Sarale Varase</h5>
+      <p class="card-text">"Sarale Varase",teaching beginners the basic swara within a particular scale and rhythm comprehension..</p>
+      
+    </div>
+  </div>
+</div>
+<div>
+        <center>
+        <h1>Video Courses</h1><br></br>
+        <iframe width="480" height="315" src="https://www.youtube.com/embed/iPbCdOsrDK4?si=wPkxKE0CNoKm1iyf" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write;  gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="480" height="315" src="https://www.youtube.com/embed/n9yqoFOY9vo?si=IjxIGXZM9V2p1QkO" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write;  gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="500" height="315" src="https://www.youtube.com/embed/ZYXsFpe-pec?si=1Zanl6sU0oF4lszr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+</center>
+    </div>
+</div>
+
+   
+  )
+}
+
+export default Carnatic
